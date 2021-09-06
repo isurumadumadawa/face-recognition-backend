651ac6c44adff6d8ff175b6bd6d8465084ab9f35
isuru-jidea <isuru@jideasolution.com>
2021-09-06 06:32:37 -0700

    initial commit

--- adapters/environmentVariables.js
+++ adapters/environmentVariables.js
@@ -1,7 +1,7 @@
 const config = require("config");
 
 exports.email = "mobiledevelopment2021@gmail.com";
-exports.password = "mit@2021";
+exports.password = "33072@isuru";
 exports.private_key = "private_key";
 exports.databaseConnectionString =
   "mongodb+srv://admin:expo@2020@cluster0.fov62.mongodb.net/exposition?retryWrites=true&w=majority";
